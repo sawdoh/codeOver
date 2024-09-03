d06f1fe35865fa899e2b3258978d5f40f1b1cc9a
sawdoh <seanhengchuanjie@gmail.com>
2024-09-03 19:26:06 +0800

    update

--- code/app/page.tsx
+++ code/app/page.tsx
@@ -47,24 +47,9 @@ export default function Home() {
     return <p>Loading...</p>;
   }
 
-  return ( signin
-    <div className="max-w-3xl space-y-4 w-full min-h-full flex flex-col items-center justify-center">
-        <h1 className="text-base sm:text-xl md:text-2xl font-medium">
-            Welcome to newspapertrade
-        </h1>
-        <h3 className="text-base sm:text-xl md:text-2xl font-medium">
-            Stop being financially illiterate
-        </h3>
-        <SignInButton mode="modal">
-            <Button>
-                Sign In
-                <ArrowRight className="h-4 w-4 ml-2"/>
-            </Button>
-        </SignInButton>
-    </div>
-);
+  return (
 
-}
+
     <div className="max-w-3xl space-y-4 w-full min-h-full flex flex-col items-center justify-center">
       <h1 className="text-base sm:text-xl md:text-2xl font-medium">
         Welcome to newspapertrade
